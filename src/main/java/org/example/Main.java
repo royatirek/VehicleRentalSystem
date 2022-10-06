852301771d21691495e837b3793a895237472073
royatirek <royatirek@gmail.com>
2022-10-06 23:46:19 +0530

    added file reading feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,20 +3,38 @@ package org.example;
 import org.example.inputcommandManagement.InputCommandFactory;
 import org.example.inputcommandManagement.commands.Command;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(true) {
-            String[] commandArguments = sc.nextLine().trim().split(" ");
-            if(commandArguments.length == 0)
-                continue;
-            if(commandArguments[0].equals("END"))
-                break;
+    public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+//        while(true) {
+//            String[] commandArguments = sc.nextLine().trim().split(" ");
+//            if(commandArguments.length == 0)
+//                continue;
+//            if(commandArguments[0].equals("END"))
+//                break;
+//            Command command = InputCommandFactory.getCommand(commandArguments[0].trim());
+//            command.setCommandArguments(commandArguments);
+//            command.execute();
+
+        String filePath = args[0];
+        final File file = new File(filePath);
+        final BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            return;
+        }
+
+        String input = reader.readLine();
+        while (input != null) {
+            String[] commandArguments = input.trim().split(" ");
             Command command = InputCommandFactory.getCommand(commandArguments[0].trim());
             command.setCommandArguments(commandArguments);
-            command.execute();
+            input = reader.readLine();
         }
     }
 }
+
